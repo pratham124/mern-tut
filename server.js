3a8f892e85c56f2c6f777ceed67ecdfa6f19d704
Pratham Sitoula <psitoula2003@gmail.com>
2023-03-25 16:14:57 -0600

    Chapter 3

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const { errorHandler } = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOption");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { Console } = require("console");
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 app.use(cors(corsOptions));
@@ -31,6 +40,18 @@ app.all("*", (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Database connection error: " + err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrlog.log"
+  );
 });
